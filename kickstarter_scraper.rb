f11e8e855809f4090fa812dc7a0b213250d6f021
Cliff Jurgensen <jivefox.002@gmail.com>
2019-01-17 22:10:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
-  # project.css("div.project-thumbnail a img").attribute("src").value
+  # image: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
 
   binding.pry
 end
